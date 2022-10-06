9316f8a497289410d8d22f0ab9a29ef69b7c82d0
Sergi Pons Freixes <sponsfreixes@users.noreply.github.com>
2022-10-05 20:06:10 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+# GitHub is particularly picky about where it finds Python packaging metadata.
+# See: https://github.com/github/feedback/discussions/6456
+#
+# To be removed once GitHub catches up.
+
+setup(
+    name="flask-htmx",
+    install_requires=[
+        "flask",
+    ],
+)
