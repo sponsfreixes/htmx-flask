d3631eee9670851cfd17937a1c46b9f294d8e35c
Sergi Pons Freixes <sponsfreixes@users.noreply.github.com>
2022-10-12 19:40:21 -0700

    Add missing constants

---
+++ src/flask_htmx/constants.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+# True and False values for the HX-* request and response headers
+HX_TRUE = "true"
+HX_FALSE = "false"
+
+# Values allowed for the HX-Reswap response header
+RESWAPS = [
+    "innerHTML",
+    "outerHTML",
+    "beforebegin" "afterbegin",
+    "beforeend",
+    "afterend",
+    "delete",
+    "none",
+]
