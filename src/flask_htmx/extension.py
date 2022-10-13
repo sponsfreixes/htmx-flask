5127b7c6c464eaa4ce3fe13645bae46b7e4457b6
Sergi Pons Freixes <sponsfreixes@users.noreply.github.com>
2022-10-12 19:40:45 -0700

    Add Flask extension

---
+++ src/flask_htmx/extension.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from flask import Flask
+
+from flask_htmx.request import HtmxAwareRequest
+
+
+class Htmx:
+    """Extension for using Flask with htmx."""
+
+    def __init__(self, app: Flask | None = None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app: Flask):
+        """
+        Initialize a Flask application for use with this extension instance. This
+        must be called before accessing ``request.htmx``.
+
+        :param app: The Flask application to initialize.
+        """
+        app.request_class = HtmxAwareRequest
+        app.extensions["htmx"] = self
